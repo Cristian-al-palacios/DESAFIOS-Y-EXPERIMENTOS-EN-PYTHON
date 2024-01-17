fd28a80acab9b153273f562966aee82607aa484e
cristian-al-palacios <cristian.al.palacios@gmail.com>
2024-01-17 01:28:55 -0300

    Cambios 17/01

--- PROYECTOS/calculadora_v0.py
+++ PROYECTOS/calculadora_v0.py
@@ -7,7 +7,6 @@ def calculadora_v0():
     operacion=(input('ingresa la operacion matematica (+ - * /)\n'))
     numero2=int(input('ingresa el segundo numero\n'))
 
-
     if operacion == '+':
         print('El resultado es:\n',(numero1 + numero2))
     elif operacion == '-':
@@ -20,12 +19,32 @@ def calculadora_v0():
         print('No selecciono un operador valido.') 
 
 
-calculadora_v0()
+#calculadora_v0()
 #no es mucho, pero es trabajo honesto (?)
 
+#voy editando un poco el codigo
+        
+        
+def calculadora_v01():
+    numero1 = int(input('Ingresa el primer numero\n'))
+    operacion = (input('ingresa la operacion matematica (+ - * /)\n'))
+    numero2 = int(input('ingresa el segundo numero\n'))
+    resultado = 'Incorrecto debido a que no selecciono un operador valido'
+
+    if operacion == '+':
+        resultado = (numero1 + numero2)
+    elif operacion == '-':
+        resultado = (numero1 - numero2)
+    elif operacion == '*':
+        resultado = (numero1 * numero2)
+    elif operacion == '/':
+        resultado = (numero1 / numero2)
+    else:
+        print('No selecciono un operador valido.') 
+
+    print(f'El resultado de {numero1} {operacion} {numero2} es: \n{resultado}')
 
 
+calculadora_v01()
 
 
-
-#print(f'El resultado de {numero1} {operacion} {numero2} es: \n {resultado}')
