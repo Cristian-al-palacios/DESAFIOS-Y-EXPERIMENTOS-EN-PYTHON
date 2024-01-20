ae4ed0c9668944d7b128316e77484a5e2375ead0
cristian-al-palacios <cristian.al.palacios@gmail.com>
2024-01-20 00:26:58 -0300

    Cambios 20/01

--- RETOS_DEV_MOURE/retos_dev_moure#4.py
+++ RETOS_DEV_MOURE/retos_dev_moure#4.py
@@ -7,26 +7,64 @@
  */
  """
 
-#En honor a la verdad, este ya lo habia hecho sin darme cuando probaba cosas en el ejercicio #1
+#como en el ejercicio #1 ya habia hecho una lista similar, en este lo modifico y agrego algunas variaciones
+
 
 def numeros_primos():
-    lista_num_primos=[f"Lista de numeros primos:"]
-    for numeros in range (1,101):
-        if 101 % numeros == 0 and numeros == 1:
-            print (str(numeros) + ' No es un numero primo')
+    for numeros in range (0,100):
+        if  numeros <= 1:
+            print (numeros , ' No es un numero primo')
         elif numeros % 2 == 0 and numeros > 2:
-            print (str(numeros) + ' No es un numero primo')
+            print (numeros , ' No es un numero primo')
         elif numeros % 3 == 0 and numeros > 3:
-            print (str(numeros) + ' No es un numero primo')
+            print (numeros , ' No es un numero primo')
         elif numeros % 5 == 0 and numeros > 5:
-            print (str(numeros) + ' No es un numero primo')
+            print (numeros , ' No es un numero primo')
         elif numeros % 7 == 0 and numeros > 7:
-            print (str(numeros) + ' No es un numero primo')
+            print (numeros , ' No es un numero primo')
         else:
-            print (str(numeros) + ' Es un numero primo')
+            print (numeros , ' Es un numero primo')
+
+numeros_primos       #devolvera solo el numero y dira si es primo o no
+
+
+def num_primos_input():
+    for numeros in range ((int(input("Ingresa el principio del rango:\n"))) , (int(input("Ingresa el fin del rango:\n")))):
+        if  numeros <= 1:
+            print (numeros , ' No es un numero primo')
+        elif numeros % 2 == 0 and numeros > 2:
+            print (numeros , ' No es un numero primo')
+        elif numeros % 3 == 0 and numeros > 3:
+            print (numeros , ' No es un numero primo')
+        elif numeros % 5 == 0 and numeros > 5:
+            print (numeros , ' No es un numero primo')
+        elif numeros % 7 == 0 and numeros > 7:
+            print (numeros , ' No es un numero primo')
+        else:
+            print (numeros , ' Es un numero primo')
+
+num_primos_input() #te pedira 2 valores para el rango y dira si es primo o no
+            
+
+def num_primos_input_lista():
+    lista_num_primos=[]
+
+    for numeros in range ((int(input("Ingresa el principio del rango:\n"))) , (int(input("Ingresa el fin del rango:\n")))):
+        if  numeros <= 1:
+            print (numeros , ' No es un numero primo')
+        elif numeros % 2 == 0 and numeros > 2:
+            print (numeros , ' No es un numero primo')
+        elif numeros % 3 == 0 and numeros > 3:
+            print (numeros , ' No es un numero primo')
+        elif numeros % 5 == 0 and numeros > 5:
+            print (numeros , ' No es un numero primo')
+        elif numeros % 7 == 0 and numeros > 7:
+            print (numeros , ' No es un numero primo')
+        else:
+            print (numeros , ' Es un numero primo')
             lista_num_primos.append(numeros)
 
-    print(lista_num_primos)
-        
+    print(lista_num_primos) 
 
-numeros_primos() 
+
+num_primos_input_lista() #pedira valores para el principio y fin del rango, dira si son o no primos y generara una lista con los que lo sean
